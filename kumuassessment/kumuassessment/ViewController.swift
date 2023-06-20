424d27e8082825a319d33a15e1e9d43b77cf398c
Lian Concepcion <liancode@yahoo.com>
2023-06-20 17:25:12 +0800

    Initial xcode commit

---
+++ kumuassessment/kumuassessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kumuassessment
+//
+//  Created by Lian Concepcion on 6/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
